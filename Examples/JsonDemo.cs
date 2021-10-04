0382ab8f38de184c95a9be042c35de40e5bf80ac
Chris Sienkiewicz <chsienki@microsoft.com>
2021-10-04 14:06:30 -0700

    Add examples

---
+++ Examples/JsonDemo.cs
@@ -0,0 +1,4 @@
+// nuget: Newtonsoft.Json
+using Newtonsoft.Json;
+
+Console.WriteLine(JsonConvert.SerializeObject(new { hello = "world" }));
---
+++ Examples/MinimalHosting.cs
@@ -0,0 +1,7 @@
+﻿//sdk: Microsoft.NET.Sdk.Web
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.Run();
